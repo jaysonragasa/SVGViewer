1f4f12737590a2caa658598ede044e958c3c5b83
Jayson Ragasa <jayson.d.ragasa@live.com>
2021-07-03 05:52:07 +0800

    Update Sat 07/03/2021 /  5:52:07.10

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace SVGViewer
+{
+    /// <summary>
+    /// SVGImage - https://github.com/dotnetprojects/SVGImage
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.AllowDrop = true;
+            this.Drop += MainWindow_Drop;
+        }
+
+        private void MainWindow_Drop(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Any())
+                {
+                    var svg = new SVGFile(files.FirstOrDefault());
+                    this.svgCanv.SetImage(svg.SVGImage);
+
+                    lblDragFileHere.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+    }
+}
---
+++ SVGFile.cs
@@ -0,0 +1,58 @@
+﻿using SVGImage.SVG;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Media;
+
+namespace SVGViewer
+{
+    public class SVGFile
+    {
+        private bool _isLogged;
+        DrawingGroup _drawing = null;
+        public SVGFile(string fullpath)
+        {
+            FullPath = fullpath;
+            Filename = Path.GetFileNameWithoutExtension(fullpath);
+        }
+
+        public string FullPath { get; private set; }
+        public string Filename { get; private set; }
+        public SVGRender SVGRender { get; private set; }
+        public DrawingGroup SVGImage
+        {
+            get
+            {
+                EnsureLoaded();
+                return _drawing;
+            }
+        }
+
+        public void Reload()
+        {
+            _drawing = SVGRender.LoadDrawing(FullPath);
+        }
+
+        void EnsureLoaded()
+        {
+            try
+            {
+                if (_drawing != null)
+                    return;
+                SVGRender = new SVGRender();
+                _drawing = SVGRender.LoadDrawing(FullPath);
+            }
+            catch (Exception ex)
+            {
+                if (_isLogged)
+                {
+                    return;
+                }
+                _isLogged = true;
+                Trace.TraceError("Exception Loading: " + this.FullPath);
+                Trace.WriteLine(ex.ToString());
+                Trace.WriteLine(string.Empty);
+            }
+        }
+    }
+}
